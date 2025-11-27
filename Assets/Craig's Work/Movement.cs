fac0071ff6f2c3332a3c77e936def96fb406350f
CraigRHarris <156079200+CraigRHarris@users.noreply.github.com>
2025-11-27 14:41:23 +0000

    fixxed errors

---
+++ Assets/Craig's Work/Movement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public float speed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+        //}
+
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+        //}
+
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
+        //}
+
+        //if (Input.GetKey(KeyCode.DownArrow))
+
+    }
+}
