428dbb5f92bc7553402442db80aa4c8d3f4db2bc
ian427 <145658104+ian427@users.noreply.github.com>
2025-12-05 11:59:25 +0000

    Balls speed up

--- Assets/Craig's Work/Spawn balls.cs
+++ Assets/Craig's Work/Spawn balls.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawnballs : MonoBehaviour
@@ -5,11 +6,20 @@ public class Spawnballs : MonoBehaviour
     [SerializeField] private GameObject Ball;
     [SerializeField] private float spawnCountdown = 0;
     [SerializeField] private int MaxSpawn, CurrentSpawned = 0;
+    [SerializeField] private int EnableSpot = 0;
+    [SerializeField] private float speedup = 0.01f;
+    [SerializeField] private List<GameObject> Balls = new List<GameObject>();
+    private List<Rigidbody2D> rbs = new List<Rigidbody2D>();
+    private bool disablespeedup = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
     void Start()
     {
         SpawnBall();
-
+        for (int i = 0; i < Balls.Count; i++)
+        {
+            rbs.Add(Balls[i].GetComponent<Rigidbody2D>());
+        }
     }
     private void SpawnBall ()
     {
@@ -18,10 +28,36 @@ public class Spawnballs : MonoBehaviour
             StartCoroutine(SpawnCounter());
         }
     }
+    private void Update()
+    {
+        if ((CurrentSpawned == 3)&&(!disablespeedup))
+        {
+            for (int i = 0; i < Balls.Count; i++)
+            {
+                if(rbs[i].gravityScale > 1)
+                {
+                    if(rbs[i] != null)
+                    {
+                        disablespeedup = true;
+                        rbs[i].gravityScale = 1;
+                    }
+                   
+                }
+                else 
+                {
+                    rbs[i].gravityScale += speedup;
 
+                }
+               
+
+            }
+        }
+    }
     System.Collections.IEnumerator SpawnCounter ()
     {
-        Instantiate(Ball, transform.position, transform.rotation);
+        Balls[EnableSpot].SetActive(true);
+        EnableSpot++;
+        //Instantiate(Ball, transform.position, transform.rotation);
         CurrentSpawned++;
         yield return new WaitForSeconds(spawnCountdown);
         SpawnBall();
