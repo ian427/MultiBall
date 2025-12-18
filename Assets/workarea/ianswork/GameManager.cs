2f604b4390d8689c4e6f9ec70788e6b9b199b30e
Ed20049 <151858732+Ed20049@users.noreply.github.com>
2025-12-18 09:39:21 +0000

    timer bug display and fix

--- Assets/workarea/ianswork/GameManager.cs
+++ Assets/workarea/ianswork/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     public DataSerilizer Data;
     public Score score;
     //[SerializeField] private int alphavalue ;
-    [SerializeField] private TextMeshProUGUI HighScoretxt;
+    [SerializeField] private TextMeshProUGUI HighScoretxt, MainHighScoretxt;
     [SerializeField] private TextMeshProUGUI CurrentScoretxt;
     //public float FadeDelay = 1f;
     //public float AlphaValue = 0;
@@ -17,12 +17,24 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField]
     private Spawnpointhandler spawners;
-   void Start()
+    void Start()
     {
         score = GetComponent<Score>();
         Data = GameObject.Find("DataHolder").GetComponent<DataSerilizer>();
         //Pannel.enabled = true;
+        int Temp;
+        Temp = Data.GetHighTime();
+        int Minutes = 0, seconds = 0;
+        while (Temp > 60)
+        {
+            Minutes -= 60;
+            Minutes++;
+
+        }
+        seconds = Temp;
+        MainHighScoretxt.text = Minutes + ":" + seconds;
     }
+        
 
 
     public void GameOver()
