95a7d435b55b9083bda3ded6313d1f33b728c4ce
ian427 <145658104+ian427@users.noreply.github.com>
2026-01-15 12:17:25 +0000

    android generated changes

---
+++ Multiball-test-mobile-01_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	11058,
+	g_Il2CppGenericTypes,
+	8055,
+	g_Il2CppGenericInstTable,
+	111093,
+	g_Il2CppGenericMethodFunctions,
+	33082,
+	g_Il2CppTypeTable,
+	126499,
+	g_Il2CppMethodSpecTable,
+	8790,
+	g_FieldOffsetTable,
+	8790,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
