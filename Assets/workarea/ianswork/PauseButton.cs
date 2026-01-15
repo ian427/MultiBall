e975518410cc7d6e4fb27144a3ef00a77318b726
ian427 <145658104+ian427@users.noreply.github.com>
2026-01-15 15:28:47 +0000

    added pause button

---
+++ Assets/workarea/ianswork/PauseButton.cs
@@ -0,0 +1,25 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    private bool ispaused = false;
+    //[SerializeField]private GameObject canvas;
+   public void Pause( )
+    {
+        if ( ispaused )
+        {
+            Time.timeScale = 1;
+            ispaused = false;
+            //canvas.SetActive( false );
+        }
+        else
+        {
+            Time.timeScale = 0;
+            ispaused = true;
+           // canvas.SetActive( true );   
+        }
+        
+
+    }
+}
